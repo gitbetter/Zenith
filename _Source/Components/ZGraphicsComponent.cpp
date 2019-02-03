b9bb8e39cbb779201fab901ed3257fbf6c9c0359
Adrian Sanchez <skatehumor@yahoo.com>
2019-02-03 16:26:34 -0500

    A slight cubemap optimization

--- _Source/Components/ZGraphicsComponent.cpp
+++ _Source/Components/ZGraphicsComponent.cpp
@@ -46,7 +46,6 @@ void ZGraphicsComponent::Update(ZCamera* camera, float frameMix) {
   // Make sure we write to the stencil buffer (if outlining is enabled, we'll need these bits)
   glStencilFunc(GL_ALWAYS, 1, 0xFF);
   glStencilMask(0xFF);
-  //if (!translatesWithView_) glDepthMask(GL_FALSE);
 
   // TODO: Set to ZGraphicsComponent transform property
   // model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.0f));
@@ -59,8 +58,6 @@ void ZGraphicsComponent::Update(ZCamera* camera, float frameMix) {
   model_->Render(shader);
 
   DrawOutlineIfEnabled();
-
-  //if (!translatesWithView_) glDepthMask(GL_TRUE);
 }
 
 void ZGraphicsComponent::SetOutline(glm::vec4 color) {
