61edc8829afb67b9f7a7ce191f3afe2f2b1748ef
Adrian Sanchez <skatehumor@yahoo.com>
2019-03-08 12:39:47 -0500

    Quick texture wrapping fix

--- _Source/Graphics/Strategies/ZGLGraphicsStrategy.cpp
+++ _Source/Graphics/Strategies/ZGLGraphicsStrategy.cpp
@@ -284,8 +284,8 @@ ZTexture ZGLGraphicsStrategy::LoadTexture(std::string path, const std::string &d
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
       glGenerateMipmap(GL_TEXTURE_2D);
 
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     } else {
