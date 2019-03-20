0b316cc6ae92ee0bc8bfbdd2578ebb7d42898aeb
Adrian Sanchez <skatehumor@yahoo.com>
2019-03-20 13:33:56 -0400

    Added GNU GPL and new file header comments

--- _Source/Graphics/ZShader.cpp
+++ _Source/Graphics/ZShader.cpp
@@ -1,10 +1,31 @@
-//
-//  ZShader.cpp
-//  Zenith
-//
-//  Created by Adrian Sanchez on 26/01/2019.
-//  Copyright © 2019 Pervasive Sense. All rights reserved.
-//
+/*
+
+   ______     ______     __   __     __     ______   __  __    
+  /\___  \   /\  ___\   /\ "-.\ \   /\ \   /\__  _\ /\ \_\ \   
+  \/_/  /__  \ \  __\   \ \ \-.  \  \ \ \  \/_/\ \/ \ \  __ \  
+    /\_____\  \ \_____\  \ \_\" \_\  \ \_\    \ \_\  \ \_\ \_\ 
+    \/_____/   \/_____/   \/_/ \/_/   \/_/     \/_/   \/_/\/_/ 
+                                                          
+    ZShader.cpp
+
+    Created by Adrian Sanchez on 26/01/2019.
+    Copyright © 2019 Pervasive Sense. All rights reserved.
+
+  This file is part of Zenith.
+
+  Zenith is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Zenith is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Zenith.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "ZShader.hpp"
 #include "ZEngine.hpp"
