028692c10d3f4dcb8d326d6cbf8bc1ef98406bb8
Adrian Sanchez <skatehumor@yahoo.com>
2019-03-31 22:58:30 -0400

    CMake now functional on macOS. Feels much better to use this build system

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make clean; make;
+cd ./_Bin; rm -rf *; cmake ..; make
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-make;
+make ./_Bin;
 
 unameOut=$(uname -s)
 case ${unameOut} in
-    Darwin*) DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:_Drivers/lib" ./game;;
-    CYGWIN*) export PATH=_Drivers/lib/:$PATH; ./game;; 
-    *) LD_LIBRARY_PATH="$LD_LIBRARY_PATH:_Drivers/lib" ./game;;
+    Darwin*) DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:_Drivers/lib" ./_Bin/zenith;;
+    CYGWIN*) export PATH=_Drivers/lib/:$PATH; ./_Bin/zenith;; 
+    *) LD_LIBRARY_PATH="$LD_LIBRARY_PATH:_Drivers/lib" ./_Bin/zenith;;
 esac
