b93664982600b3868c08bee6d2f148ba13cfda44
Adrian <skatehumor@yahoo.com>
2019-04-01 17:24:37 -0400

    A few changes to the build process to preserve directory structure in Visual Studio projects

--- rebuild.ps1
+++ rebuild.ps1
@@ -2,4 +2,4 @@ if (Test-Path _Bin -PathType Container) {
     rm -r -force _Bin/;
 }
 
-mkdir _Bin; cd _Bin; cmake -G "Visual Studio 15 2017 Win64" ..; cd ..;
+mkdir _Bin; cd _Bin; cmake -G "Visual Studio 15 2017 Win64" ..; explorer .; cd ..;
