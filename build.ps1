dbbdfcd7ba26e0bcd4defcd931ab17f257183d88
Adrian <skatehumor@yahoo.com>
2019-04-07 10:16:23 -0400

    Added development switch to build.ps1 script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,19 @@
+param(
+    [switch]$development=$false
+)
+
 if (-not (Test-Path _Bin -PathType Container)) {
-    powershell .\rebuild.ps1;
+    if ($development) {
+        powershell .\rebuild.ps1 -development;
+    } else {
+	powershell .\rebuild.ps1;
+    }
 } else {
-    cd _Bin; cmake -G "Visual Studio 15 2017 Win64" ..; cd ..;
+    cd _Bin;
+    if ($development) {
+        cmake -G "Visual Studio 15 2017 Win64" -DDEVELOPMENT=ON ..;
+    } else {
+        cmake -G "Visual Studio 15 2017 Win64" ..;
+    }
+    cd ..;
 }
